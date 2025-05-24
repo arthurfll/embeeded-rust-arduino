58758dacfb1acc41d70914ab7e64f62489814c49
João Arthur Gonçalves Fontenelle <arthurfontenelle2017@gmail.com>
2025-05-24 12:45:09 -0300

    desafio1

--- Sandbox/Dotnet/Program.cs
+++ Sandbox/Dotnet/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("?");
+
+string foo;
+var resposta = Console.ReadLine();
+
+
+if (resposta != null)
+{
+  foo = resposta;
+} else { foo = "nuloo"; }
+
+Console.WriteLine($"Resposta:{foo}");
+
