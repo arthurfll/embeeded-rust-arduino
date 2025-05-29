aa7f03b542e4477bb66876e440f6af5f67ecdcbb
João Arthur Gonçalves Fontenelle <arthurfontenelle2017@gmail.com>
2025-05-29 17:34:31 -0300

    ok

--- Sandbox/Dotnet/obj/Debug/net9.0/Dotnet.AssemblyInfo.cs
+++ Sandbox/Dotnet/obj/Debug/net9.0/Dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3d48269643ce84cc082e2aee17531c6c30b0444")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58758dacfb1acc41d70914ab7e64f62489814c49")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
